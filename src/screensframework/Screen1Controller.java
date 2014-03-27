841139d36e407338b5e66e526da49becf4fc9f72
gabrielitium <gabrielreyesev@gmail.com>
2014-03-26 19:39:16 -0600

    commit

---
+++ src/screensframework/Screen1Controller.java
@@ -0,0 +1,29 @@
+package screensframework;
+
+import javafx.fxml.Initializable;
+
+public class Screen1Controller implements Initializable, ControlledScreen {
+
+    ScreensController myController;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+    
+    public void setScreenParent(ScreensController screenParent){
+        myController = screenParent;
+    }
+
+    @FXML
+    private void goToScreen2(ActionEvent event){
+       myController.setScreen(ScreensFramework.screen2ID);
+    }
+    
+    @FXML
+    private void goToScreen3(ActionEvent event){
+       myController.setScreen(ScreensFramework.screen3ID);
+    }
+}
